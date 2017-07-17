3a59b51cb38b2a792f83c34ce29270eb9ebe8be3
hopen <yhp9523@qq.com>
2017-07-17 12:14:33 +0800

    new repository

--- TF1D.hpp
+++ TF1D.hpp
@@ -48,36 +48,75 @@ public:
         }
 
         this->setup();
+        fclose(fp);
     }
 
     void writeToFile(std::string filename) {
+        fp = fopen(filename.c_str(), "r");
+        if (fp == NULL) {
+            std::cout << "Cannot open file '" << filename << "' !" << std::endl;
+            return;
+        }
 
+        fprintf(fp, "%d %f %f\n", num, valueRange.x, valueRange.y);
+        for (int i = 0; i < num; i++) {
+            fprintf(fp, "%f %f %f %f %f %f %f %f %f",
+                    &index, &color.x, &color.y, &color.z, &opacity,
+                    &color.x, &color.y, &color.z, &opacity);
+        }
+        fclose(fp);
     }
 
     void setup() {
-        float opacity;
-        osp::vec3f color_front, color_rear;
-        float index, index_front, index_rear;
 
         int item_num = 256;
         int steps;
 
         ospRelease(tf1d);
         tf1d = ospNewTransferFunction("piecewise_linear");
-        osp::vec3f colors_tmp[item_num];
-        osp::vec3f opacities_tmp[item_num];
+
+
+        float ratio_front, ratio_rear;
+        float index, index_front, index_rear;
+        float opacity, opacity_front, opacity_rear;
+        osp::vec3f color, color_front, color_rear;
+
+        std::vector<osp::vec3f> colors_tmp;
+        std::vector<float> opacities_tmp;
 
         int curr = 0;
         for (int i = 0; i < item_num; i++) {
-            index = (float) i / item_num * (valueRange.y - valueRange.x);
+            index = valueRange.x + (float) i / item_num * (valueRange.y - valueRange.x);
             index_front = indexs[curr];
-            index_rear = curr == num - 1 ? valueRange.y : indexs[curr + 1];
+            index_rear = curr < num - 1 ? indexs[curr + 1] : valueRange.y;
+            if (index > index_rear) {
+                curr++;
+                continue;
+            }
             color_front = colors[curr];
-            color
+            color_rear = colors[curr < num - 1 ? curr + 1 : 0];
+            opacity_front = colors[curr];
+            opacities_rear = colors[curr < num - 1 ? curr + 1 : 0];
+
+            ratio_front = (index - index_front) / (index_rear - index_front);
+            ratio_rear = 1 - ratio_front;
+
+            color = ratio_front * color_rear + ratio_rear * color_front;
+            opacity = ratio_front * opacity_rear + ratio_rear * opacity_front;
+
+            colors_tmp.push_back(color);
+            opacities_tmp.push_back(opacity);
         }
 
-        colors.clear();
+        OSPData colors_data = ospNewData(item_num, OSP_FLOAT3, colors_tmp.data());
+        ospCommit(colors_data);
+        OSPData opacities_data = ospNewData(item_num, OSP_FLOAT, opacities_tmp.data());
+        ospCommit(opacities_data);
 
+        ospSetData(tf1d, "colors", colors_data);
+        ospSetData(tf1d, "opacities", opacities_data);
+
+        ospCommit(tf1d);
     }
 
 private:
